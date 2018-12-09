b1ba9f68056d3ab8d32c1a21e749ed303e06e4b8
dragoonzx <dragoonz@yandex.ru>
2018-12-09 18:19:11 +0300

    fix

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -14,7 +14,7 @@ namespace BubbleSort
 
             for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 1; j < array.Length - i - 1; j++)
+                for (int j = 0; j < array.Length - i - 1; j++)
                 {
                     if (array[j] > array[j+1])
                     {

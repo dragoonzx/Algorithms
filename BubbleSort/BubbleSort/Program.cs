7c493935a8f4e3cdc1ce7a621b3208def49e42de
dragoonzx <dragoonz@yandex.ru>
2018-12-09 18:17:25 +0300

    Bubble sort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 5, 6, 3, 8 };
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 1; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        array[j] += array[j + 1];
+                        array[j + 1] = array[j] - array[j + 1];
+                        array[j] -= array[j + 1];
+                    }
+                }
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
